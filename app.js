2fc036416be46702d33a34268fe1b9ec95dc5f4c
VARUN-KUSH <shiva92637@gmail.com>
2024-05-10 12:44:30 +0530

    chnages

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const express = require('express');
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import { mapScrapper } from "./controller";
+dotenv.config({
+    path: './.env'
+})
+
+const PORT = 9000;
 
 const app = express()
-const PORT = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.get("/api/v1/home/healthtest", (req, res) => {
+    res.send('all good')
+  })
+app.post("/api/v1/home/datascrape", mapScrapper)
+
+app.listen(() => {
+    console.log(`⚙️ Server is running at port `);
 })
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
-})
+// http://localhost:8000/api/v1/home/
 
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-})
+export {app}
